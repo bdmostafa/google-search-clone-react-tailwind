39098151bc7dd90f54d19621c5605e3b22649060
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-10-25 10:15:33 +0600

    footer ui

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Heart } from "../assets/icons/heart.js";
 
 export const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="text-center mt-10 border-t p-10 border-gray-200 dark:border-gray-700">
+      Copyright © 2021. All Rights Reserved. Made <Heart /> by with{" "}
+      <a href="https://mdmostafa.com" target="_blank" rel="noreferrer">
+        Mostafa
+      </a>
+    </div>
+  );
+};
