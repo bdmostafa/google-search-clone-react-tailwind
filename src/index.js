dc5d49b5dde9506c2efc4e1e63436c51cfb29038
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-10-25 12:14:11 +0600

    result context provider, loading spinner created and used in results component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./global.css";
 import App from "./App";
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from "react-router-dom";
+import { ResultContextProvider } from "./contexts/ResultContextProvider";
 
 ReactDOM.render(
-<Router>
-    <App />
-</Router>
-, document.getElementById("root"));
+  <ResultContextProvider>
+    <Router>
+      <App />
+    </Router>
+  </ResultContextProvider>,
+  document.getElementById("root")
+);
